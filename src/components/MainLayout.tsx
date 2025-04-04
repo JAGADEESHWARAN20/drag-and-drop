89fcbb72573c913ffa10d8b81e62f0920f623124
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-04 17:58:59 +0530

    continueing coding

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -109,7 +109,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({
                               </SheetTrigger>
                               <SheetContent
                                    side="top"
-                                   className={`w-full h-auto max-h-96 p-4 flex flex-col ${isMobile ? 'w-[100vw]' : 'min-w-[300px]'}`}
+                                   className={`w-full h-auto max-h-96 p-4 flex flex-col overflow-x-scroll scrollbar-hidden ${isMobile ? 'min-w-[100vw]' : 'min-w-[300px]'}`}
                               >
                                    <h2 className="text-lg font-semibold text-blue-900 dark:text-blue-300 mb-2">
                                         Component Library
@@ -117,7 +117,7 @@ const MainLayout: React.FC<MainLayoutProps> = ({
                                    <div className="relative">
                                         <div
                                              ref={componentPanelScrollRef}
-                                             className="pb-2 scroll-componentcontainer"
+                                             className="pb-2 scroll-componentcontainer overflow-x-scroll"
                                         >
                                              <ComponentPanel ref={componentPanelRef} />
                                         </div>
