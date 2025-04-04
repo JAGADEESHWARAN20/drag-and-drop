c159adcb232ea38a91ad95ef0eb4afe467c48193
JAGADEESHWARAN20 <jagadeeshwaransp5@gmail.com>
2025-04-04 16:50:24 +0530

    continueing coding

--- src/components/ComponentPanel.tsx
+++ src/components/ComponentPanel.tsx
@@ -63,9 +63,11 @@ const ComponentPanel = forwardRef<HTMLDivElement, ComponentPanelProps>(({ }, ref
             <h3 className="text-sm font-medium mb-2 text-gray-600 dark:text-gray-400 uppercase">
               {category}
             </h3>
-            <div className="flex flex-col flex-wrap sm:flex-row overflow-x-auto md:overflow-y-auto">
+            <div className="sm:grid sm:grid-cols-1 md:grid-cols-2 gap-2 sm:overflow-x-auto md:overflow-y-auto sm:flex-row">
               {components.map((component) => (
-                <DraggableComponent key={component.type} component={component} />
+                <div key={component.type} className="sm:inline-block">
+                  <DraggableComponent component={component} />
+                </div>
               ))}
             </div>
           </div>
